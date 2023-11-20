059ac795cd596c7ba63e8e8b6626a2836df957e9
Dmytro Stas <dima7stas777@gmail.com>
2023-11-19 23:21:23 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
-dotenv.config({ path: ".env" });
+dotenv.config();
 
 const { DB_HOST } = process.env;
 
